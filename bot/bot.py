a59fda623a194a77515f3729c77befd7d1f30466
amirdianov <amirdianov@gmail.com>
2024-11-30 17:10:25 +0300

    Task-1. Инициализация обработки текста и фотографии

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types
+from aiogram import F, Bot, Dispatcher, types
 from aiogram.filters.command import Command
 import os
 
@@ -27,6 +27,23 @@ async def cmd_start(message: types.Message):
 @dp.message(Command("help"))
 async def cmd_start(message: types.Message):
     await message.answer("Я помогу подготовить ответ на экзаменационный вопрос по дисциплине 'Операционные системы'. \nПришлите мне фотографию с вопросом или наберите его текстом.")
+
+# Обработчики для текстовых сообщений
+@dp.message(F.text)
+async def handle_text_message(message: types.Message):
+    question = message.text
+    await message.reply(f"Скоро научусь обрабатывать текстовые сообщения по типу: {question}")
+
+# Обработчик для фотографий
+@dp.message(F.photo)
+async def handle_photo(message: types.Message):
+    await message.reply(f"Фотография")
+
+# Обработчик для других типов сообщений
+@dp.message()
+async def handle_other(message: types.Message):
+    await message.reply("Я могу обработать только текстовое сообщение или фотографию.")
+
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
