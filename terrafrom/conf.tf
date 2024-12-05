7235fc3c9ff93fdf0812563c29b204fa331b5668
amirdianov <amirdianov@gmail.com>
2024-12-05 14:25:22 +0300

    Task-1. Добавлена выгрузка в бакет

--- terrafrom/conf.tf
+++ terrafrom/conf.tf
@@ -13,6 +13,16 @@ provider "yandex" {
   service_account_key_file = "/home/ubuntu/tf-key/key.json"
 }
 
+resource "yandex_storage_bucket" "bucket" {
+  bucket = "vvot-25-hw1-bucket"
+}
+
+resource "yandex_storage_object" "text_config" {
+  bucket = yandex_storage_bucket.bucket.id
+  source = "/home/ubuntu/hw/hw_1/text_config.txt"
+  key    = "instruction.txt"
+}
+
 variable "CLOUD_ID" {
   type        = string
   description = "Идентификатор облака"
