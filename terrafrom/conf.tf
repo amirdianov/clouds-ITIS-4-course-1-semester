3e811f10db659015849d4642cc6066c0c18bfa6c
amirdianov <amirdianov@gmail.com>
2024-12-05 14:29:03 +0300

    Task-1. Добавлена функция и архивация

--- terrafrom/conf.tf
+++ terrafrom/conf.tf
@@ -23,6 +23,32 @@ resource "yandex_storage_object" "text_config" {
   key    = "instruction.txt"
 }
 
+resource "yandex_function" "function" {
+  name       = "vvot-25-hw1-function"
+  user_hash  = data.archive_file.zip.output_sha256
+  runtime    = "python312"
+  entrypoint = "bot.handler"
+  service_account_id = "ajebfdo7fm8mv1mfl91q"
+  memory     = 128
+  execution_timeout = 30
+  environment = {
+    "YANDEX_ACCESS_KEY" = var.YANDEX_ACCESS_KEY,
+    "YANDEX_SECRET_KEY" = var.YANDEX_SECRET_KEY,
+    "CLOUD_ID"          = var.CLOUD_ID,
+    "FOLDER_ID"         = var.FOLDER_ID,
+    "TG_BOT_KEY"        = var.TG_BOT_KEY
+  }
+  content {
+    zip_filename = data.archive_file.zip.output_path
+  }
+}
+
+data "archive_file" "zip" {
+  type        = "zip"
+  output_path = "bot.zip"
+  source_dir  = "src_new"
+}
+
 variable "CLOUD_ID" {
   type        = string
   description = "Идентификатор облака"
@@ -31,4 +57,19 @@ variable "CLOUD_ID" {
 variable "FOLDER_ID" {
   type        = string
   description = "Идентификатор папки"
+}
+
+variable "YANDEX_ACCESS_KEY" {
+  type        = string
+  description = "Идентификатор статичного ключа"
+}
+
+variable "YANDEX_SECRET_KEY" {
+  type        = string
+  description = "Секретный ключ"
+}
+
+variable "TG_BOT_KEY" {
+  type        = string
+  description = "Бот ключ"
 }
